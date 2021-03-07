2bd3da83917439486ec31e37228e95ce08ab3062
E1e5en <doksergey@gmail.com>
2021-03-07 15:43:15 +0300

    New version
    - Поворот готов

---
+++ FlowerGarden2/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1615120795,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/cell-sheet0.png",
+		"images/cell-sheet1.png",
+		"images/border-sheet0.png",
+		"images/connect-sheet0.png",
+		"images/connect-sheet1.png",
+		"images/pipe_i2-sheet0.png",
+		"images/pipe_i3-sheet0.png",
+		"images/pipe_i4-sheet0.png",
+		"images/point_rotate-sheet0.png",
+		"images/sprinkler_out_1-sheet0.png",
+		"images/logo_jam-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
