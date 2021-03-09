74844774e7653eb8c97422b9321e5dd52f8435d1
E1e5en <doksergey@gmail.com>
2021-03-09 22:34:48 +0300

    New version

---
+++ FlowerGarden3/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1615318385,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/cell-sheet0.png",
+		"images/cell-sheet1.png",
+		"images/border-sheet0.png",
+		"images/point_start-sheet0.png",
+		"images/point_finish-sheet0.png",
+		"images/connect-sheet0.png",
+		"images/connect-sheet1.png",
+		"images/point_rotate-sheet0.png",
+		"images/btn_rotate-sheet0.png",
+		"images/pipe_no_rotate-sheet0.png",
+		"images/pipe_rotate-sheet0.png",
+		"images/pipe_rotate-sheet1.png",
+		"images/sprinkler-sheet0.png",
+		"images/logo_jam-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
