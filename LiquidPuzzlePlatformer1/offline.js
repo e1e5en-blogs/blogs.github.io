5825640c12a7bd2eb222494c2e18e6706d5daa73
E1e5en <doksergey@gmail.com>
2021-04-07 11:52:00 +0300

    LiquidPuzzlePlatformer - Prototype
    LiquidPuzzlePlatformer

---
+++ LiquidPuzzlePlatformer1/offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1617785318,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tm_bg_wall.png",
+		"images/tm_fluid.png",
+		"images/tm_platfrom.png",
+		"images/s_platform_jump-sheet0.png",
+		"images/s_platform_jump-sheet1.png",
+		"images/s_env_grid-sheet0.png",
+		"images/s_env_grid-sheet1.png",
+		"images/s_legs_top-sheet0.png",
+		"images/s_legs_w_c-sheet0.png",
+		"images/s_panel-sheet0.png",
+		"images/s_panel-sheet1.png",
+		"images/ply_skin-sheet0.png",
+		"images/s_box-sheet0.png",
+		"images/s_water-sheet0.png",
+		"images/s_water_phy-sheet0.png",
+		"images/p_water_1.png",
+		"images/s_cup-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
